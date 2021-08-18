76269d0bb130ce20f2ce31bfc0a842ffbe49cba2
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-18 15:00:35 +0530

    [Rupali]Add:implemented stack and queue

--- LinkedListStack.cs
+++ LinkedListStack.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace StackAndQueue
 {
-    class Class1
+    public class LinkedListStack
     {
+        Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+            Console.Write(" [" + node.data + "] →");
+        }
     }
 }
--- Node.cs
+++ Node.cs
@@ -6,5 +6,11 @@ namespace StackAndQueue
 {
     class Node
     {
+        internal int data;
+        internal Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
     }
 }
